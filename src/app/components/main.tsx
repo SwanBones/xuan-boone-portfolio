d526b428828b793bc91956d690f49b757e90db1d
SwanBones <xuan.boone@gmail.com>
2025-01-06 13:48:14 +0100

    Navbar and content

---
+++ src/app/components/main.tsx
@@ -0,0 +1,10 @@
+import Hero from "./hero";
+import WebDev from "./webdev";
+export default function Main() {
+	return (
+		<div>
+			<Hero />
+			<WebDev />
+		</div>
+	);
+}
---
+++ src/app/components/webdev.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "../styles/page.module.css";
+import specificStyles from "../styles/specific-styles.module.css";
+
+export default function Hero() {
+	return (
+		<div className={styles.webDev}>
+			<h2>Web Development</h2>
+			<p>
+				Étudiant en dernière année d’Ingénieur en Informatique et Systèmes
+				d'Information À l’UTT
+			</p>
+			<p>Actuellement en recherche de stage | Stage de fin d’études</p>
+			<div className={styles.ctas}>
+				<a
+					className={styles.primary}
+					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					En savoir plus
+				</a>
+			</div>
+		</div>
+	);
+}
